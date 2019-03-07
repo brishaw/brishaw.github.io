bb18878421e5560e05b68c53c923bd6f617f3f24
Brian <40767783+brishaw@users.noreply.github.com>
2019-03-07 12:34:06 -0500

    working with a cors issue...

--- js/scripts.js
+++ js/scripts.js
@@ -48,9 +48,69 @@ window.onload = function () {
       new TxtType(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
+
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".who-am-i > .wrap { border-right: 0.08em solid #fff}";
+  css.innerHTML = ".who-am-i > .wrap { border-right: 0.09em solid #fff}";
   document.body.appendChild(css);
 };
+
+// modal controller
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("recycledInfo");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// ajax for info
+function ajax_get(url, callback) {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function () {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      console.log('responseText:' + xmlhttp.responseText);
+      try {
+        var data = JSON.parse(xmlhttp.responseText);
+      } catch (err) {
+        console.log(err.message + " in " + xmlhttp.responseText);
+        return;
+      }
+      callback(data);
+    }
+  };
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+ajax_get('info.json', function (data) {
+  document.getElementById("title").innerHTML = data["title"];
+
+  var html = "<h2>" + data["title"] + "</h2>";
+  html += "<h3>" + data["description"] + "</h3>";
+  html += "<ul>";
+  for (var i = 0; i < data["articles"].length; i++) {
+    html += '<li><a href="' + data["articles"][i]["url"] + '">' + data["articles"][i]["title"] + "</a></li>";
+  }
+  html += "</ul>";
+  document.getElementById("id01").innerHTML = html;
+});
